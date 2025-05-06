970599f5355d3a571ed1bf46ccfc249be5234d92
kamalesh <kamaleshbaskaran4310@gmail.com>
2025-05-06 14:23:25 +0530

    updated LLM

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Langchain-LLM-Model
-Langchain Demo
+##Local LLaMA 3 Demo with Streamlit & LangChain
+
+Welcome to a fun little demo that brings the power of LLaMA 3, an open-source language model, right to your local machine! This project uses Streamlit to create a simple web app where you can ask any question or type in a topic, and the app will give you a simple explanation, all powered by LangChain and hosted locally using Ollama.
--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import os 
-from constant import API_KEY
-from langchain_google_genai import GoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain   
+from langchain_community.llms import Ollama
 import streamlit as st
 
-st.title("Langchain demo with Gemini 2.0 Flash")
-input_txt = st.text("Search What you want")
 
-llm = GoogleGenerativeAI(
-    model = "google-flash-2.0",
-    google_api_key = API_KEY
-)
+st.title("Langchain Demo with LLaMA 3 (Local Via Ollama)")
+input_txt = st.text_input("Enter a topic you want explained")
 
-response = llm.invoke(input_txt)
+llm = Ollama(model="llama3")
 
-prompt = PromptTemplate(
+#response = llm.invoke(input_txt)
+
+
+prompt_template = PromptTemplate(
     input_variables=['topic'],
     template = "Explain {topic} in simple terms."
 )
 
 chains = LLMChain(
     llm = llm,
-    prompt = prompt
+    prompt = prompt_template
 )
-output = chains.run("black Holes")
-print(output)
+
+output = chains.run(input_txt)
+
+st.subheader("Response: ")
+st.write(output)
